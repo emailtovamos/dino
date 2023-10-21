f3c97a29fa81eb7e6b5e570bd6524c06389f7635
Satyajit <emailtovamos@gmail.com>
2023-10-21 21:17:26 +0100

    correct contract

--- dino/contracts/token.sol
+++ dino/contracts/token.sol
@@ -1,14 +1,27 @@
-// contracts/Token.sol
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Token is ERC20 {
-  constructor() ERC20('GameToken', 'GT') {
-    _mint(msg.sender, 1000000 * (10 ** decimals()));
-  }
+contract ScoreToken is ERC20 {
+    address public gameAdmin;
+    mapping(address => uint256) public scores;
 
-  function mint(address to, uint256 amount) external {
-    _mint(to, amount);
-  }
+    modifier onlyAdmin() {
+        require(msg.sender == gameAdmin, "Not admin");
+        _;
+    }
+
+    constructor() ERC20("ScoreToken", "SCT") {
+        gameAdmin = msg.sender;
+    }
+
+    function setScore(address player, uint256 score) external onlyAdmin {
+        scores[player] = score;
+        _mint(player, score * 10);  // Reward player with 10 tokens per score point
+    }
+
+    function getScore(address player) external view returns (uint256) {
+        return scores[player];
+    }
 }
